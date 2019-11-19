87084940bb0c8bec3ce7d07a90dc9986b26a40a6
Jesse Tippit <jtippit223@gmail.com>
2019-11-19 03:12:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,12 +29,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  arry5 = 
+  arry5 = ["wow", "I", "am", "really", "learning", "arrays!"]
   arry5[2]
 end
 
 def retrieve_first_element_from_array(array)
-  arry6 = [1, 2 , 3]
+  arry6 = 
   arry6[0]
 end
 
